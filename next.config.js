f6e34fa433ee3e719df48eb465d03e81e286db38
Kotla Madhu <kotlamadhu0614@gmail.com>
2025-06-25 19:23:21 +0530

    Remove server files for static build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
